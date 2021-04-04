f780afe922a15b0168c50c27caa1670c10ec176b
Conni <connithekiwi@gmail.com>
2021-04-04 18:26:59 +0100

    Stuff

--- Utils/api/controllers/AuthGateways.js
+++ Utils/api/controllers/AuthGateways.js
@@ -3,12 +3,29 @@ const codes = require("../../error_codes").codes;
 
 const mongoose = require('mongoose');
 const Members = mongoose.model('Members');
+const Guilds = mongoose.model('Guilds');
 
 const logging = require('../../logging');
 
 exports.login = (req, res) => {
-    logging.log(`Trying to login as. ${req.params.MemberID}`);
-    console.log({MemberID: req.params.MemberID, hash: req.body.hash});
+    logging.log(`Trying to login as. ${req.params.MemberID} - providing hash: -${req.body.hash}-`);
+    Members.find({id: req.params.MemberID, hash: req.body.hash}, (err, Response) => {
+        if (err) {
+            res.status(codes.Bad_Request);
+            res.send(err);
+        }
+        if(Response.length == 0) {
+            res.status(codes.Not_Found);
+            res.send(err);
+        } else {
+            res.status(codes.Ok);
+            res.json(Response);
+        }
+  });
+};
+
+exports.reauth = (req, res) => {
+    logging.log(`Trying to login as. ${req.params.MemberID} - providing hash: -${req.body.hash}-`);
     Members.find({MemberID: req.params.MemberID, hash: req.body.hash}, (err, Response) => {
         if (err) {
             res.status(codes.Bad_Request);
