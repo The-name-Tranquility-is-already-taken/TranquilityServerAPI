9350223489375f7bf33b7e83e7a1d1e84b3b7a60
Conni <connithekiwi@gmail.com>
2021-04-10 15:56:17 +0100

    Fix schemes

--- api/models/MemberModel.js
+++ api/models/MemberModel.js
@@ -1,10 +1,10 @@
 "use strict";
 const mongoose = require("mongoose");
-// const Snowflake = require("../../Utils/snowflake").GenerateID;
+const Snowflake = require("../../Utils/snowflake").GenerateID;
 
 const Schema = mongoose.Schema;
 const MemberSchema = new Schema({
-  id: { type: String, default: "Snowflake" },
+  id: { type: String, default: Snowflake },
   createdDate: { type: Date, default: Date.now },
   tag: { type: String, required: "A user tag is needed" },
   hash: { type: String, required: "Hash of password is needed" },
