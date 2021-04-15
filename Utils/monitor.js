0471842a5f5b95e29d8acceafa99a537f3da6a99
Restyled.io <commits@restyled.io>
2021-04-15 16:35:24 +0000

    Restyled by clang-format

--- Utils/monitor.js
+++ Utils/monitor.js
@@ -1,130 +1,130 @@
 const template = (name_t) => ({
-    name: name_t,
-    data: [],
-    averageTimes: {
-        all: -1, // Time in ms
-    },
-    totalTime: 0,
-    callCount: 0,
+  name : name_t,
+  data : [],
+  averageTimes : {
+    all : -1, // Time in ms
+  },
+  totalTime : 0,
+  callCount : 0,
 });
 
 var data = [];
 
 module.exports.output = () => {
-    data.forEach((entry) => {
-        console.log("-----------------------");
-        console.log(entry.name);
-        console.log(`Average Times: ${entry.averageTimes.all}ms`);
-        console.log(`Total Time: ${entry.totalTime}ms`);
-        console.log(`Called: ${entry.callCount} times`);
-    });
+  data.forEach((entry) => {
+    console.log("-----------------------");
+    console.log(entry.name);
+    console.log(`Average Times: ${entry.averageTimes.all}ms`);
+    console.log(`Total Time: ${entry.totalTime}ms`);
+    console.log(`Called: ${entry.callCount} times`);
+  });
 };
 
 function getSpecificDataSet(name) {
-    var Times = [];
-    var Ms = [];
-    data.forEach(entry => {
-        // console.log("Entry name: ", entry.name, " - ", name);
-        if (entry.name == name) {
-            // console.log("Pushing");
+  var Times = [];
+  var Ms = [];
+  data.forEach(entry => {
+    // console.log("Entry name: ", entry.name, " - ", name);
+    if (entry.name == name) {
+      // console.log("Pushing");
 
-            var i = 0;
-            entry.data.forEach(r => {
-                Times.push(entry.data[i].timeStamp);
-                Ms.push(entry.data[i].timeTaken);
-                ++i;
-            });
-        }
-    });
+      var i = 0;
+      entry.data.forEach(r => {
+        Times.push(entry.data[i].timeStamp);
+        Ms.push(entry.data[i].timeTaken);
+        ++i;
+      });
+    }
+  });
 
-    // console.log(data);
-    // console.log([ Times, Ms ]);
+  // console.log(data);
+  // console.log([ Times, Ms ]);
 
-    return ([Times, Ms]);
+  return ([ Times, Ms ]);
 }
 
 module.exports.data = (req, res) => {
-    var colours = [{
-        bg: 'rgb(255, 99, 132)',
-        border: 'rgb(255, 99, 132)'
-    }];
+  var colours = [ {bg : 'rgb(255, 99, 132)', border : 'rgb(255, 99, 132)'} ];
 
-    var curColour = 0;
+  var curColour = 0;
 
-    function createDataSet(label, times) {
-        return ({
-            label: label,
-            backgroundColor: colours[curColour].bg,
-            borderColor: colours[curColour].border,
-            data: times,
+  function createDataSet(label, times) {
+    return ({
+      label : label,
+      backgroundColor : colours[curColour].bg,
+      borderColor : colours[curColour].border,
+      data : times,
 
-        })
-    }
+    })
+  }
 
-    var all = [];
+  var all = [];
 
-    var dat = getSpecificDataSet(req.body.target);
-    var times = dat[0];
+  var dat = getSpecificDataSet(req.body.target);
+  var times = dat[0];
 
-    //console.log(times);
-    var dataSet = createDataSet(req.body.target, getSpecificDataSet(req.body.target)[1]);
-    //console.log(dataSet);
+  // console.log(times);
+  var dataSet =
+      createDataSet(req.body.target, getSpecificDataSet(req.body.target)[1]);
+  // console.log(dataSet);
 
-    all.push(dataSet);
+  all.push(dataSet);
 
-    //console.log("All:",all[0]);
+  // console.log("All:",all[0]);
 
-    res.json({ all: all[0], times: times });
+  res.json({all : all[0], times : times});
 };
 
-module.exports.log = async(module_t, timeTaken) => {
-    timeTaken = parseInt(timeTaken);
+module.exports.log = async (module_t, timeTaken) => {
+  timeTaken = parseInt(timeTaken);
 
-    var i = 0;
-    var found = false;
-    data.forEach((e) => {
-        if (e.name == module_t) {
-            var shouldHalf = false;
-            if (data[i].averageTimes.all != -1) shouldHalf = true;
+  var i = 0;
+  var found = false;
+  data.forEach((e) => {
+    if (e.name == module_t) {
+      var shouldHalf = false;
+      if (data[i].averageTimes.all != -1)
+        shouldHalf = true;
 
-            // Submit data
-            data[i].data.push({ timeTaken: timeTaken, timeStamp: getDateTime() });
-            data[i].averageTimes.all += timeTaken + 1;
-            data[i].totalTime += timeTaken;
-            data[i].callCount += 1;
+      // Submit data
+      data[i].data.push({timeTaken : timeTaken, timeStamp : getDateTime()});
+      data[i].averageTimes.all += timeTaken + 1;
+      data[i].totalTime += timeTaken;
+      data[i].callCount += 1;
 
-            if (shouldHalf) {
-                data[i].averageTimes.all /= 2;
-            }
-            found = true;
-        }
-        ++i;
-    });
-    if (!found) {
-        data.push(template(module_t));
-        this.log(module_t, timeTaken);
+      if (shouldHalf) {
+        data[i].averageTimes.all /= 2;
+      }
+      found = true;
     }
+    ++i;
+  });
+  if (!found) {
+    data.push(template(module_t));
+    this.log(module_t, timeTaken);
+  }
 };
 
 function getDateTime() {
-    var date = new Date();
+  var date = new Date();
 
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
+  var hour = date.getHours();
+  hour = (hour < 10 ? "0" : "") + hour;
 
-    var min = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
+  var min = date.getMinutes();
+  min = (min < 10 ? "0" : "") + min;
 
-    var sec = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
+  var sec = date.getSeconds();
+  sec = (sec < 10 ? "0" : "") + sec;
 
-    // var year = date.getFullYear();
+  // var year = date.getFullYear();
 
-    // var month = date.getMonth() + 1;
-    // month = (month < 10 ? "0" : "") + month;
+  // var month = date.getMonth() + 1;
+  // month = (month < 10 ? "0" : "") + month;
 
-    // var day = date.getDate();
-    // day = (day < 10 ? "0" : "") + day;
+  // var day = date.getDate();
+  // day = (day < 10 ? "0" : "") + day;
 
-    return /*year + ":" + month + ":" + day + " - " + */ hour + ":" + min + "." + sec;
+  return /*year + ":" + month + ":" + day + " - " + */ hour + ":" + min + "." +
+         sec;
 }
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,69 +1,49 @@
 "use strict";
 module.exports = (app) => {
-    const MemberList = require("../gatewayFunctions/memberGateway");
-    const GuildGatewayController = require("../gatewayFunctions/guildGateway");
-    const AuthGateways = require("../gatewayFunctions/authGateway");
-    const auth = require("../proxys/authProxy").authWrapper;
-    const monitoring = require("./../../Utils/monitor");
+  const MemberList = require("../gatewayFunctions/memberGateway");
+  const GuildGatewayController = require("../gatewayFunctions/guildGateway");
+  const AuthGateways = require("../gatewayFunctions/authGateway");
+  const auth = require("../proxys/authProxy").authWrapper;
+  const monitoring = require("./../../Utils/monitor");
 
+  /**
+   *  Un-Authenticated Routes Routes
+   */
 
+  app.route("/api/member/register").post(MemberList.createNewMember);
 
-    /**
-     *  Un-Authenticated Routes Routes
-     */
+  app.route("/api/member/login").get(MemberList.login);
 
-    app
-        .route("/api/member/register")
-        .post(MemberList.createNewMember);
+  /**
+   *  Authenticated Routes
+   */
+  app.route("/api/member/:MemberID")
+      .get(auth, MemberList.getMemberRecord)
+      .put(auth, MemberList.updateMember)
+      .delete(auth, MemberList.deleteMember);
 
-    app
-        .route("/api/member/login")
-        .get(MemberList.login);
+  // Routes for getting all guilds a user has access to. and creating guilds.
+  app.route("/api/guild/:MemberID")
+      .get(auth, GuildGatewayController.getGuildsUserCanAccess)
+      .post(auth, GuildGatewayController.createGuild);
 
+  // Routes for joining guilds.
+  app.route("/api/guild/:MemberID/:GuildID/:GuildInvite")
+      .get(GuildGatewayController.joinGuild);
 
-    /**
-     *  Authenticated Routes
-     */
-    app
-        .route("/api/member/:MemberID")
-        .get(auth, MemberList.getMemberRecord)
-        .put(auth, MemberList.updateMember)
-        .delete(auth, MemberList.deleteMember);
+  /*
+    Authentication gateway for authentication
+  */
+  app.route("/api/auth/:MemberID").get(AuthGateways.login);
 
-    // Routes for getting all guilds a user has access to. and creating guilds.
-    app
-        .route("/api/guild/:MemberID")
-        .get(auth, GuildGatewayController.getGuildsUserCanAccess)
-        .post(auth, GuildGatewayController.createGuild);
+  // app.route("/api/auth/:MemberID/verify/phone")
+  //     .post(AuthGateways.phoneVerif);
+  // app.route("/api/auth/:MemberID/verify/email")
+  //     .post(AuthGateways.emailVerif);
 
-    // Routes for joining guilds.
-    app
-        .route("/api/guild/:MemberID/:GuildID/:GuildInvite")
-        .get(GuildGatewayController.joinGuild);
+  /**
+   * Monitoring API
+   */
 
-
-
-
-    /*
-      Authentication gateway for authentication
-    */
-    app.route("/api/auth/:MemberID")
-        .get(AuthGateways.login);
-
-
-    // app.route("/api/auth/:MemberID/verify/phone")
-    //     .post(AuthGateways.phoneVerif);
-    // app.route("/api/auth/:MemberID/verify/email")
-    //     .post(AuthGateways.emailVerif);
-
-
-
-    /**
-     * Monitoring API
-     */
-
-    app.route("/api/monitoring/data")
-        .get(monitoring.data);
-
-
+  app.route("/api/monitoring/data").get(monitoring.data);
 };
